1ae43f06fbe56ea36414fc5e56e6a78cc7e9e512
Jean-Luc Déziel <jean-luc.deziel@leddartech.com>
2020-12-02 12:16:48 -0500

    feat: add signal to noise ration methods for echo and trace samples

--- pioneer/das/api/samples/echo.py
+++ pioneer/das/api/samples/echo.py
@@ -5,11 +5,13 @@ from pioneer.das.api.samples.sample import Sample
 import copy
 import numpy as np
 
+
 class Echo(Sample):
     """Sample from a single data package provided by a LCAx sensor. 
         See pioneer.common.clouds.to_echo_package() to create a similar data package from scratch.
     """
-    def __init__(self, index, datasource, virtual_raw = None, virtual_ts = None):
+
+    def __init__(self, index, datasource, virtual_raw=None, virtual_ts=None):
         super(Echo, self).__init__(index, datasource, virtual_raw, virtual_ts)
         self._mask = None
 
@@ -20,23 +22,27 @@ class Echo(Sample):
             r['das.sample'] = self
             if r['data']['timestamps'][-1] == 0:
                 try:
-                    cfg = self.datasource.sensor['cfg'].get_at_timestamp(self.timestamp).raw
+                    cfg = self.datasource.sensor['cfg'].get_at_timestamp(
+                        self.timestamp).raw
                     if self.datasource.sensor.specs is None:
-                        self.datasource.sensor.specs = banks.extract_specs(lambda n: cfg[n])
-                    
+                        self.datasource.sensor.specs = banks.extract_specs(
+                            lambda n: cfg[n])
+
                     try:
                         if self.datasource.sensor.modules_angles is None:
-                            self.datasource.sensor.modules_angles = banks.extract_intrinsics_modules_angles(lambda n: cfg[n])
+                            self.datasource.sensor.modules_angles = banks.extract_intrinsics_modules_angles(
+                                lambda n: cfg[n])
                     except:
-                        LoggingManager.instance().warning("Sensor {} has no modules angles, or can not read modules angles from the sensor intrinsic calibratrion".format(self.datasource.sensor.name))
+                        LoggingManager.instance().warning(
+                            "Sensor {} has no modules angles, or can not read modules angles from the sensor intrinsic calibratrion".format(self.datasource.sensor.name))
 
-                    banks.add_timestamp_offsets(r, self.datasource.sensor.name, self.datasource.sensor.specs, \
-                        int(cfg['ID_ACCUMULATION_EXP']), int(cfg['ID_OVERSAMPLING_EXP']), int(cfg['ID_BASE_POINT_COUNT']))
+                    banks.add_timestamp_offsets(r, self.datasource.sensor.name, self.datasource.sensor.specs,
+                                                int(cfg['ID_ACCUMULATION_EXP']), int(cfg['ID_OVERSAMPLING_EXP']), int(cfg['ID_BASE_POINT_COUNT']))
                 except:
                     pass
-                    
+
             self._raw = r
-            
+
         return self._raw
 
     @property
@@ -61,16 +67,19 @@ class Echo(Sample):
             data = self.raw['data']
             config = self.datasource.sensor.config
 
-            vmask = np.bitwise_and(data['flags'], 0x01).astype(np.bool) #keep valid echoes only
-            fmask = np.isin(data['flags'], config['reject_flags'], invert = True) & vmask
+            vmask = np.bitwise_and(data['flags'], 0x01).astype(
+                np.bool)  # keep valid echoes only
+            fmask = np.isin(
+                data['flags'], config['reject_flags'], invert=True) & vmask
 
             def get_mask(slices, values):
                 dmask = np.full_like(fmask, True)
-                for s,e in slices:
+                for s, e in slices:
                     dmask = dmask & ~((values >= s) & (values <= e))
                 return dmask
-            
-            self._mask = fmask & get_mask(config['dist_reject_intervals'], data['distances']) & get_mask(config['amp_reject_intervals'], data['amplitudes'])
+
+            self._mask = fmask & get_mask(config['dist_reject_intervals'], data['distances']) & get_mask(
+                config['amp_reject_intervals'], data['amplitudes'])
 
         return self._mask
 
@@ -90,9 +99,9 @@ class Echo(Sample):
 
     @property
     def timestamps(self):
-        try:#if relative timestamps are converted to global, there will be a key 'timestamps' in the raw
+        try:  # if relative timestamps are converted to global, there will be a key 'timestamps' in the raw
             t_ = self.raw['timestamps'][self.mask]
-        except: #otherwise global timestamps are considered to be directly in the data
+        except:  # otherwise global timestamps are considered to be directly in the data
             t_ = self.raw['data']['timestamps'][self.mask]
         return t_
 
@@ -114,14 +123,15 @@ class Echo(Sample):
     def cache(self):
         return self.datasource.sensor.cache(self.specs)
 
-    def get_cloud(self, referential:str = None, ignore_orientation:bool=False, undistort:bool=False, reference_ts:int=-1, dtype:np.dtype = np.float64):
-        points, amplitudes, triangles = self.quad_cloud(referential, ignore_orientation, undistort, reference_ts, dtype)
+    def get_cloud(self, referential: str = None, ignore_orientation: bool = False, undistort: bool = False, reference_ts: int = -1, dtype: np.dtype = np.float64):
+        points, amplitudes, triangles = self.quad_cloud(
+            referential, ignore_orientation, undistort, reference_ts, dtype)
         return points, amplitudes, triangles.reshape(-1, 3)
 
-    def point_cloud(self, referential:str=None, ignore_orientation:bool=False, undistort:bool=False, reference_ts:int=-1, 
-                                dtype:np.dtype=np.float64):
+    def point_cloud(self, referential: str = None, ignore_orientation: bool = False, undistort: bool = False, reference_ts: int = -1,
+                    dtype: np.dtype = np.float64):
         """Compute a 3D point cloud from raw data
-        
+
         Args:
             referential: The target sensor referential or full datasource name
             ignore_orientation: Ignore the source sensor orientation (default: {False})
@@ -130,15 +140,17 @@ class Echo(Sample):
             dtype: the output numpy data type
         """
 
-        pts_Local = self.datasource.sensor.get_corrected_cloud(self.timestamp, self.cache(), 'point_cloud', self.indices, self.distances, None, dtype)
-        
+        pts_Local = self.datasource.sensor.get_corrected_cloud(
+            self.timestamp, self.cache(), 'point_cloud', self.indices, self.distances, None, dtype)
+
         if undistort:
             to_world = referential == 'world'
-            self.undistort_points([pts_Local], self.timestamps, reference_ts, to_world, dtype = dtype)
+            self.undistort_points(
+                [pts_Local], self.timestamps, reference_ts, to_world, dtype=dtype)
             if to_world:
-                return pts_Local # note that in that case, orientation has to be ignored
+                return pts_Local  # note that in that case, orientation has to be ignored
 
-        return self.transform(pts_Local, referential, ignore_orientation, reference_ts, dtype = dtype)
+        return self.transform(pts_Local, referential, ignore_orientation, reference_ts, dtype=dtype)
 
     def quad_cloud(self, referential=None, ignore_orientation=False, undistort=False, reference_ts=-1, dtype=np.float64):
         """Compute a 3d surface cloud from raw data (quads made of 2 triangles)
@@ -160,28 +172,32 @@ class Echo(Sample):
 
             # four points per quad, 1 different direction per point, same distance for each
             to_world = referential == 'world'
-            self.undistort_points([pts_Local[0:sn], pts_Local[sn:2*sn], pts_Local[2*sn:3*sn], pts_Local[3*sn:]]
-            , self.timestamps, reference_ts, to_world, dtype = dtype)
+            self.undistort_points([pts_Local[0:sn], pts_Local[sn:2*sn], pts_Local[2*sn:3*sn],
+                                   pts_Local[3*sn:]], self.timestamps, reference_ts, to_world, dtype=dtype)
             if to_world:
-                return pts_Local, quad_amplitudes, quad_indices # note that in that case, orientation has to be ignored
+                # note that in that case, orientation has to be ignored
+                return pts_Local, quad_amplitudes, quad_indices
 
-        pts_Ref = self.transform(pts_Local, referential, ignore_orientation, reference_ts, dtype = dtype)
+        pts_Ref = self.transform(
+            pts_Local, referential, ignore_orientation, reference_ts, dtype=dtype)
         return pts_Ref, quad_amplitudes, quad_indices
 
-    def amplitude_img(self, options='max_amplitude', dtype = np.float32, extrema = None):
+    def amplitude_img(self, options='max_amplitude', dtype=np.float32, extrema=None):
 
         if extrema is None:
             extrema = self.datasource.sensor.config['extrema_amp']
 
         if options == 'max_amplitude':
-            img = images.extrema_image(self.v, self.h, self.data, sort_field='amplitudes', sort_direction=-1, dtype=dtype, extrema=extrema)
+            img = images.extrema_image(
+                self.v, self.h, self.data, sort_field='amplitudes', sort_direction=-1, dtype=dtype, extrema=extrema)
         elif options == 'min_distance':
-            _, others = images.extrema_image(self.v, self.h, self.data, sort_field='distances', sort_direction=1, 
-                other_fields=['amplitudes'], dtype=dtype, extrema=extrema)
+            _, others = images.extrema_image(self.v, self.h, self.data, sort_field='distances', sort_direction=1,
+                                             other_fields=['amplitudes'], dtype=dtype, extrema=extrema)
             img = others['amplitudes']
 
         elif options == 'amplitudes_sum':
-            img = images.accumulation_image(self.v, self.h, indices=self.data['indices'], weights=self.data['amplitudes'], dtype=dtype)
+            img = images.accumulation_image(
+                self.v, self.h, indices=self.data['indices'], weights=self.data['amplitudes'], dtype=dtype)
         else:
             raise ValueError('Invalid options: {}'.format(options))
 
@@ -193,22 +209,24 @@ class Echo(Sample):
             extrema = self.datasource.sensor.config['extrema_dist']
 
         if options == 'max_amplitude':
-            _, others = images.extrema_image(self.v, self.h, self.data, sort_field='amplitudes', 
-                sort_direction=-1, other_fields=['distances'], dtype=dtype, extrema=extrema)
+            _, others = images.extrema_image(self.v, self.h, self.data, sort_field='amplitudes',
+                                             sort_direction=-1, other_fields=['distances'], dtype=dtype, extrema=extrema)
             img = others['distances']
 
         elif options == 'min_distance':
-            img = images.extrema_image(self.v, self.h, self.data, sort_field='distances', sort_direction=1, dtype=dtype, extrema=extrema)
+            img = images.extrema_image(
+                self.v, self.h, self.data, sort_field='distances', sort_direction=1, dtype=dtype, extrema=extrema)
         elif options == 'distances_sum':
-            img = images.accumulation_image(self.v, self.h, indices=self.data['indices'], weights=self.data['distances'], dtype=dtype)
+            img = images.accumulation_image(
+                self.v, self.h, indices=self.data['indices'], weights=self.data['distances'], dtype=dtype)
         else:
             raise ValueError('Invalid options: {}'.format(options))
 
         return self.transform_image(img)
 
-    def other_field_img(self, field, dtype = np.float32):
-        amp, others = images.extrema_image(self.v, self.h, self.data, sort_field='amplitudes', 
-            sort_direction=-1, other_fields=[field], dtype=dtype, extrema=None)
+    def other_field_img(self, field, dtype=np.float32):
+        amp, others = images.extrema_image(self.v, self.h, self.data, sort_field='amplitudes',
+                                           sort_direction=-1, other_fields=[field], dtype=dtype, extrema=None)
         img = others[field]
 
         return self.transform_image(img)
@@ -217,7 +235,8 @@ class Echo(Sample):
     def coords_img_tf(self):
         if not hasattr(self, '_coords_img_tf'):
             vv, hh = np.mgrid[0:self.v, 0:self.h]
-            coords_img = np.stack((vv,hh, np.arange(0, self.v*self.h).reshape(self.v, self.h)), axis=2)
+            coords_img = np.stack(
+                (vv, hh, np.arange(0, self.v*self.h).reshape(self.v, self.h)), axis=2)
             self._coords_img_tf = self.transform_image(coords_img)
         return self._coords_img_tf
 
@@ -225,9 +244,9 @@ class Echo(Sample):
         return self.coords_img_tf[row, col, 2]
 
     def channel_index_to_image_coord(self, index):
-        return np.argwhere(self.coords_img_tf[...,2] == index)[0]
+        return np.argwhere(self.coords_img_tf[..., 2] == index)[0]
 
-    def image_stack(self, field:str, number_images:int=3, options='min_distance', dtype=np.float32):
+    def image_stack(self, field: str, number_images: int = 3, options='min_distance', dtype=np.float32):
         """Returns multiple images to account for the multiple echoes per channel"""
         image_stack = np.zeros((number_images, self.v, self.h))
         remaining_data = copy.deepcopy(self.data)
@@ -238,40 +257,41 @@ class Echo(Sample):
                 mask = images.echoes_visibility_mask(remaining_data)
             elif options == 'max_amplitude':
                 mask = images.maximum_amplitude_mask(remaining_data)
-            
-            image_stack[i] = self.transform_image(images.extrema_image(self.v, self.h, remaining_data[mask], 
-                sort_direction=1, other_fields=[field], dtype=dtype)[1][field])
+
+            image_stack[i] = self.transform_image(images.extrema_image(self.v, self.h, remaining_data[mask],
+                                                                       sort_direction=1, other_fields=[field], dtype=dtype)[1][field])
             remaining_data = remaining_data[~mask]
         return image_stack
 
+    def clip_to_fov_mask(self, pts: np.ndarray) -> np.ndarray:
 
-    def clip_to_fov_mask(self, pts:np.ndarray) -> np.ndarray:
-                
         lcax = self.datasource.sensor
         specs = self.specs
 
         if lcax.angle_chart:
             cache = self.cache()
-            correct_v_angles = lcax.get_corrected_projection_data(self.timestamp, cache, 'angles')
+            correct_v_angles = lcax.get_corrected_projection_data(
+                self.timestamp, cache, 'angles')
 
             v_cell_size, h_cell_size = clouds.v_h_cell_size_rad(specs)
 
-            planes = clouds.frustrum_planes(clouds.custom_frustrum_directions(correct_v_angles, v_cell_size, h_cell_size, dtype = np.float64))
+            planes = clouds.frustrum_planes(clouds.custom_frustrum_directions(
+                correct_v_angles, v_cell_size, h_cell_size, dtype=np.float64))
         else:
-            planes = clouds.frustrum_planes(clouds.frustrum_directions(specs['v_fov'], specs['h_fov'], dtype = np.float64))
+            planes = clouds.frustrum_planes(clouds.frustrum_directions(
+                specs['v_fov'], specs['h_fov'], dtype=np.float64))
 
         if self.orientation is not None:
             planes[:, :3] = (self.orientation @ (planes[:, :3].T)).T
 
         return plane.plane_test(planes[0], pts)\
-                & plane.plane_test(planes[1], pts)\
-                & plane.plane_test(planes[2], pts)\
-                & plane.plane_test(planes[3], pts)
+            & plane.plane_test(planes[1], pts)\
+            & plane.plane_test(planes[2], pts)\
+            & plane.plane_test(planes[3], pts)
 
-
-    def get_rgb_from_camera_projection(self, camera:str, undistort:bool=False, return_mask:bool=False):
+    def get_rgb_from_camera_projection(self, camera: str, undistort: bool = False, return_mask: bool = False):
         """Returns the rgb data for each point from its projected position in camera.
-        
+
             Args:
                 camera: (str) name of the camera datasource (ex: 'flir_bbfc_flimg')
                 undistort: (bool) if True, motion compensation is applied to the points before the projection (default is False)
@@ -282,22 +302,24 @@ class Echo(Sample):
                 mask (optional): a Nx1 array of booleans. Values are True where points are inside the camera fov. False elsewhere.
         """
 
-        image_sample = self.datasource.sensor.pf[camera].get_at_timestamp(self.timestamp)
+        image_sample = self.datasource.sensor.pf[camera].get_at_timestamp(
+            self.timestamp)
 
-        pcloud = self.point_cloud(referential=platform.extract_sensor_id(camera), undistort=undistort)
-        projection, mask = image_sample.project_pts(pcloud, mask_fov=True, output_mask=True)
+        pcloud = self.point_cloud(
+            referential=platform.extract_sensor_id(camera), undistort=undistort)
+        projection, mask = image_sample.project_pts(
+            pcloud, mask_fov=True, output_mask=True)
         projection = projection.astype(int)
 
         rgb = np.zeros((pcloud.shape[0], 3))
         image = image_sample.raw_image()
-        rgb[mask,:] = image[projection[:,1], projection[:,0]]
+        rgb[mask, :] = image[projection[:, 1], projection[:, 0]]
 
         if return_mask:
             return rgb, mask
         return rgb
 
-
-    def get_pulses(self, trace_ds_type:str, pulse_sample_size:int=10, trace_processing=None, return_distance_scaling:bool=False):
+    def get_pulses(self, trace_ds_type: str, pulse_sample_size: int = 10, trace_processing=None, return_distance_scaling: bool = False):
         """Returns the corresponding pulses for every echoes from a trace datasource.
 
             Args:
@@ -308,19 +330,34 @@ class Echo(Sample):
                 trace_processing: (callable) function applied to the traces.
                 return_distance_scaling: (bool) If True, also return the distance (meters) between trace data points.
         """
-        trace_sample = self.datasource.sensor[trace_ds_type].get_at_timestamp(self.timestamp)
-        traces = trace_sample.raw if trace_processing is None else trace_sample.processed(trace_processing)
+        trace_sample = self.datasource.sensor[trace_ds_type].get_at_timestamp(
+            self.timestamp)
+        traces = trace_sample.raw if trace_processing is None else trace_sample.processed(
+            trace_processing)
         if 'high' in traces:
             traces = traces['high']
 
         full_traces = traces['data'][self.indices]
 
-        echoes_positions_in_traces = ((self.distances - traces['time_base_delays'][self.indices])/traces['distance_scaling']).astype(int)
+        echoes_positions_in_traces = (
+            (self.distances - traces['time_base_delays'][self.indices])/traces['distance_scaling']).astype(int)
         ind = np.indices(echoes_positions_in_traces.shape)
-        padded_traces = np.pad(full_traces,((0,0),(pulse_sample_size,pulse_sample_size+1)))
-        
-        pulses = np.vstack([padded_traces[ind,echoes_positions_in_traces+ind_pulse+pulse_sample_size] for ind_pulse in np.arange(-pulse_sample_size, pulse_sample_size+1, dtype=int)]).T
+        padded_traces = np.pad(
+            full_traces, ((0, 0), (pulse_sample_size, pulse_sample_size+1)))
+
+        pulses = np.vstack([padded_traces[ind, echoes_positions_in_traces+ind_pulse+pulse_sample_size]
+                            for ind_pulse in np.arange(-pulse_sample_size, pulse_sample_size+1, dtype=int)]).T
 
         if return_distance_scaling:
             return pulses, traces['distance_scaling']
         return pulses
+
+    def get_signal_to_noise(self, trace_ds_type: str):
+        traces = self.datasource.sensor[trace_ds_type].get_at_timestamp(
+            self.timestamp).raw
+        if 'high' in traces:
+            traces = traces['high']
+        traces_zeroed = traces['data'] - \
+            np.mean(traces['data'], axis=1)[:, None]
+        noise = np.std(traces_zeroed, axis=1)[self.indices]
+        return np.log10(self.amplitudes**2/noise)+1
--- pioneer/das/api/samples/fast_trace.py
+++ pioneer/das/api/samples/fast_trace.py
@@ -6,23 +6,26 @@ from typing import Callable
 import copy
 import numpy as np
 
+
 class FastTrace(Trace):
     """Derivation of Trace sample, for Pixell sensors. 
         Two distinct sets of waveforms are contained in the raw data dictionnary, under the keys 'high' and 'low'.
     """
 
-    def __init__(self, index, datasource, virtual_raw = None, virtual_ts = None):
-        super(FastTrace, self).__init__(index, datasource, virtual_raw, virtual_ts)
+    def __init__(self, index, datasource, virtual_raw=None, virtual_ts=None):
+        super(FastTrace, self).__init__(
+            index, datasource, virtual_raw, virtual_ts)
 
     @property
     def raw(self):
         if self._raw is None:
             self._raw = super(Trace, self).raw
 
-            for fast_trace_type in ['low','high']:
+            for fast_trace_type in ['low', 'high']:
                 self._raw[fast_trace_type]['time_base_delays'] = self.datasource.sensor.time_base_delays[fast_trace_type]
                 self._raw[fast_trace_type]['distance_scaling'] = self.datasource.sensor.distance_scaling
-                self._raw[fast_trace_type]['trace_smoothing_kernel'] = self.datasource.sensor.get_trace_smoothing_kernel()[fast_trace_type]
+                self._raw[fast_trace_type]['trace_smoothing_kernel'] = self.datasource.sensor.get_trace_smoothing_kernel()[
+                    fast_trace_type]
 
         return self._raw
 
@@ -33,23 +36,24 @@ class FastTrace(Trace):
         rawMid = self.raw['high']['data']
         array = np.zeros((2, specs['v']*specs['h'], rawMid.shape[-1]))
         array[0] = rawMid
-        array[1,:,:rawLow.shape[-1]] = rawLow
+        array[1, :, :rawLow.shape[-1]] = rawLow
         return array.reshape((2, specs['v'], specs['h'], array.shape[-1]))
 
-    def processed_array(self, trace_processing:Callable):
+    def processed_array(self, trace_processing: Callable):
         specs = self.specs
         processedLow = self.processed(trace_processing)['low']['data']
         processedMid = self.processed(trace_processing)['high']['data']
         array = np.zeros((2, specs['v']*specs['h'], processedMid.shape[-1]))
         array[0] = processedMid
-        array[1,:,:processedLow.shape[-1]] = processedLow
+        array[1, :, :processedLow.shape[-1]] = processedLow
         return array.reshape((2, specs['v'], specs['h'], array.shape[-1]))
 
-    def processed(self, trace_processing:Callable):
+    def processed(self, trace_processing: Callable):
         processed_traces = {}
         raw_copy = copy.deepcopy(self.raw)
-        for fast_trace_type in ['low','high']:
-            processed_traces[fast_trace_type] = trace_processing(raw_copy[fast_trace_type])
+        for fast_trace_type in ['low', 'high']:
+            processed_traces[fast_trace_type] = trace_processing(
+                raw_copy[fast_trace_type])
         return processed_traces
 
     @property
@@ -57,3 +61,13 @@ class FastTrace(Trace):
         raw = self.raw
         trace_lenght = raw['high']['data'].shape[-1]
         return raw['high']['time_base_delays'] + trace_lenght*raw['high']['distance_scaling']
+
+    @property
+    def signal_to_noise(self):
+        snr = {}
+        for fast_trace_type in ['low', 'high']:
+            traces_zeroed = self.raw[fast_trace_type]['data'] - \
+                np.mean(self.raw[fast_trace_type]['data'], axis=1)[:, None]
+            snr[fast_trace_type] = np.log10(
+                np.mean(traces_zeroed**2, axis=1)/np.std(traces_zeroed, axis=1))+1
+        return snr
