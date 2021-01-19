b322a1c401d2099d37211440ca155497a76a5441
JF Parent <jf.parent@leddartech.com>
2021-01-19 15:14:41 -0500

    feat: modify box2d and seg2d annotations for instance segmentation

--- pioneer/das/api/samples/annotations/box_2d.py
+++ pioneer/das/api/samples/annotations/box_2d.py
@@ -1,6 +1,17 @@
 from pioneer.das.api.samples.sample import Sample
+from pioneer.das.api import categories
+from pioneer.common import platform as platform_utils
+from pioneer.common.logging_manager import LoggingManager
+
 
 class Box2d(Sample):
-
     def __init__(self, index, datasource, virtual_raw = None, virtual_ts = None):
         super(Box2d, self).__init__(index, datasource, virtual_raw, virtual_ts)
+
+    def label_names(self):
+        '''Converts the category numbers in their corresponding names (e.g. 0 -> 'pedestrian') and returns the list of names for all boxes in the sample'''
+        label_source_name = categories.get_source(platform_utils.parse_datasource_name(self.datasource.label)[2])
+        try:
+            return [categories.CATEGORIES[label_source_name][str(category_number)]['name'] for category_number in self.raw['data']['classes']]
+        except:
+            LoggingManager.instance().warning(f"Can not find the CATEGORIES and NAMES of {label_source_name}.")
--- pioneer/das/api/samples/annotations/seg_2d.py
+++ pioneer/das/api/samples/annotations/seg_2d.py
@@ -6,9 +6,10 @@ import copy
 import cv2
 import numpy as np
 
+
 class Seg2d(Sample):
 
-    def __init__(self, index, datasource, virtual_raw = None, virtual_ts = None):
+    def __init__(self, index, datasource, virtual_raw=None, virtual_ts=None):
         super(Seg2d, self).__init__(index, datasource, virtual_raw, virtual_ts)
 
     def poly2d(self, confidence_threshold=0.5):
@@ -16,27 +17,34 @@ class Seg2d(Sample):
         raw = copy.deepcopy(self.raw)
 
         poly2d = np.empty(0, dtype=datatypes.poly2d())
-        for seg_data in raw['data']:
-            mask = (seg_data['confidences']>confidence_threshold).astype(np.uint8)
-            if mask.max()==0:
+
+        box_confidences = None
+        if 'box_confidence' in raw.keys():
+            box_confidences = raw['box_confidence']
+
+        for i, seg_data in enumerate(raw['data']):
+            if box_confidences is not None and box_confidences[i] < confidence_threshold:
                 continue
-            polys,_ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            mask = (seg_data['confidences'] > confidence_threshold).astype(np.uint8)
+            if mask.max() == 0:
+                continue
+            polys, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             polys = np.array(polys)
             for poly in polys:
-                poly = poly[:,0,:]
-                poly2d = np.append(poly2d, np.array([(poly,seg_data['classes'],0,0)], dtype=datatypes.poly2d()))
+                poly = poly[:, 0, :]
+                poly2d = np.append(poly2d, np.array([(poly, seg_data['classes'], 0, 0)], dtype=datatypes.poly2d()))
 
         return poly2d
 
-    def mask_category(self, category:str, resolution:tuple=None, confidence_threshold:float=0.5):
+    def mask_category(self, category: str, resolution: tuple = None, confidence_threshold: float = 0.5):
 
-        _,_,ds_type = platform.parse_datasource_name(self.datasource.label)
+        _, _, ds_type = platform.parse_datasource_name(self.datasource.label)
         source = categories.get_source(ds_type)
-        
+
         mask = np.zeros((self.raw['data']['confidences'][0].shape))
 
         for seg_data in self.raw['data']:
-            name,_ = categories.get_name_color(source, seg_data['classes'])
+            name, _ = categories.get_name_color(source, seg_data['classes'])
             if name == category:
                 above_threshold = np.where(seg_data['confidences'] >= confidence_threshold)
                 mask[above_threshold] = 1
@@ -46,5 +54,4 @@ class Seg2d(Sample):
 
     @staticmethod
     def resize_mask(mask, resolution):
-        return cv2.resize(mask, (resolution[1],resolution[0]), interpolation = cv2.INTER_NEAREST)
-                
+        return cv2.resize(mask, (resolution[1], resolution[0]), interpolation=cv2.INTER_NEAREST)
