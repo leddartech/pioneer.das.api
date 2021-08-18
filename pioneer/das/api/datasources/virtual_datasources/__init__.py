804e73a53ede4c6954037c4318b920a1cd62b3c9
Jean-Luc Déziel <jean-luc.deziel@leddartech.com>
2021-08-18 10:23:32 -0400

    feat: point cloud fusion virtual datasource

--- pioneer/das/api/datasources/virtual_datasources/__init__.py
+++ pioneer/das/api/datasources/virtual_datasources/__init__.py
@@ -3,6 +3,7 @@ from pioneer.das.api.datasources.virtual_datasources.cylindrical_projection impo
 from pioneer.das.api.datasources.virtual_datasources.echoes_from_traces import Echoes_from_Traces
 from pioneer.das.api.datasources.virtual_datasources.echoes_with_pulses import EchoesWithPulses
 from pioneer.das.api.datasources.virtual_datasources.hdr_waveform import HDRWaveform
+from pioneer.das.api.datasources.virtual_datasources.point_cloud_fusion import PointCloudFusion
 from pioneer.das.api.datasources.virtual_datasources.rgb_cloud import RGBCloud
 from pioneer.das.api.datasources.virtual_datasources.waveform_cloud import WaveformCloud
 from pioneer.das.api.datasources.virtual_datasources.voxel_map import VoxelMap
@@ -12,6 +13,7 @@ VIRTUAL_DATASOURCE_FACTORY = {
     'echoes_from_traces': Echoes_from_Traces,
     'echoes_with_pulses': EchoesWithPulses,
     'hdr_waveform': HDRWaveform,
+    'point_cloud_fusion': PointCloudFusion, 
     'rgb_cloud': RGBCloud,
     'waveform_cloud': WaveformCloud,
     'voxel_map': VoxelMap,
---
+++ pioneer/das/api/datasources/virtual_datasources/point_cloud_fusion.py
@@ -0,0 +1,65 @@
+from pioneer.das.api.datasources.virtual_datasources.virtual_datasource import VirtualDatasource
+from pioneer.das.api.datatypes import datasource_xyzit
+
+from typing import Any
+
+import numpy as np
+
+
+class PointCloudFusion(VirtualDatasource):
+    """Merges multiple points clouds in a single one"""
+
+    def __init__(self, reference_sensor:str, dependencies:list):
+        """Constructor
+            Args:
+                reference_sensor (str): The name of the reference sensor (e.g. 'pixell_bfc').
+                dependencies (list): A list of the point cloud datasources to fuse. 
+        """
+        ds_type = 'xyzit-fused'
+        super(PointCloudFusion, self).__init__(ds_type, dependencies, None)
+        self.reference_sensor = reference_sensor  
+        self.reference_datasource = dependencies[0]  
+
+    def get_at_timestamp(self, timestamp):
+        sample = self.datasources[self.reference_datasource].get_at_timestamp(timestamp)
+        return self[int(np.round(sample.index))]
+
+    @staticmethod
+    def stack_point_cloud(stack, point_cloud):
+        return np.vstack([stack, point_cloud])
+
+    def clear_cache(self):
+        self.local_cache = None
+
+    def __getitem__(self, key:Any):
+
+        pcloud_fused = np.empty((0,5))
+
+        for ds_name in self.dependencies:
+            sample = self.datasources[ds_name][key]
+            xyz = sample.point_cloud(referential=self.reference_sensor)
+
+            if self.sensor.orientation is not None:
+                xyz = xyz @ self.sensor.orientation
+
+            pcloud = np.empty((xyz.shape[0],5))
+            pcloud[:,[0,1,2]] = xyz
+            pcloud[:,3] = sample.amplitudes
+            pcloud[:,4] = sample.timestamps
+
+            pcloud_fused = self.stack_point_cloud(pcloud_fused, pcloud)
+
+        #package in das format
+        dtype = datasource_xyzit()
+        raw = np.empty((pcloud_fused.shape[0]), dtype=dtype)
+        raw['x'] = pcloud_fused[:,0]
+        raw['y'] = pcloud_fused[:,1]
+        raw['z'] = pcloud_fused[:,2]
+        raw['i'] = pcloud_fused[:,3]
+        raw['t'] = pcloud_fused[:,4]
+
+        sample_object = self.sensor.factories['xyzit'][0]
+        ts = self.datasources[self.dependencies[0]][key].timestamp
+
+        return sample_object(index=key, datasource=self, virtual_raw=raw, virtual_ts=ts)
+
