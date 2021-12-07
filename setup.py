7325bedc9dc991aff4d83fecc0d4f52f9be60344
Xavier Ouimet <xavier.ouimet@leddartech.com>
2021-12-07 13:44:21 -0500

    feat: changed version for release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_reqs = parse_requirements('requirements.txt')
 
 setuptools.setup(
     name="pioneer_das_api", # Replace with your own username
-    version="1.2.0",
+    version="1.3.0",
     author="Leddartech",
     description="Pioneer's das api",
     packages=[
