ba45aa0284cf8e560e5b3fa32e87d0ef4d15ea51
Jean-Luc Déziel <jean-luc.deziel@leddartech.com>
2022-02-24 22:46:01 +0000

    added bounding_box_metadata virtual datasource

--- pioneer/das/api/datasources/virtual_datasources/__init__.py
+++ pioneer/das/api/datasources/virtual_datasources/__init__.py
@@ -1,4 +1,5 @@
 from pioneer.das.api.datasources.virtual_datasources.virtual_datasource import VirtualDatasource
+from pioneer.das.api.datasources.virtual_datasources.bounding_box_metadata import BoundingBoxMetadata
 from pioneer.das.api.datasources.virtual_datasources.cylindrical_projection import FlirCylindricalProjection
 from pioneer.das.api.datasources.virtual_datasources.echoes_from_traces import Echoes_from_Traces
 from pioneer.das.api.datasources.virtual_datasources.echoes_with_pulses import EchoesWithPulses
@@ -9,6 +10,7 @@ from pioneer.das.api.datasources.virtual_datasources.waveform_cloud import Wavef
 from pioneer.das.api.datasources.virtual_datasources.voxel_map import VoxelMap
 
 VIRTUAL_DATASOURCE_FACTORY = {
+    'bounding_box_metadata': BoundingBoxMetadata,
     'cylindrical_projection': FlirCylindricalProjection,
     'echoes_from_traces': Echoes_from_Traces,
     'echoes_with_pulses': EchoesWithPulses,
---
+++ pioneer/das/api/datasources/virtual_datasources/bounding_box_metadata.py
@@ -0,0 +1,50 @@
+from pioneer.das.api.datasources.virtual_datasources.virtual_datasource import VirtualDatasource
+from pioneer.das.api.samples import Sample
+
+from typing import Any, List, Optional
+
+import numpy as np
+
+
+class BoundingBoxMetadata(VirtualDatasource):
+    """Extract metadata from bounding boxes"""
+
+    def __init__(self, reference_sensor:str, dependencies:List[str], category_counts:Optional[List[str]]=None):
+        """Constructor
+            Args:
+                reference_sensor (str): The name of the reference sensor (e.g. 'pixell_bfc').
+                dependencies (list): A list of the bounding boxes datasources. 
+        """
+        ds_type = 'scalars'
+        super(BoundingBoxMetadata, self).__init__(ds_type, dependencies, None)
+        self.reference_sensor = reference_sensor  
+        self.reference_datasource = dependencies[0]
+        self.category_counts = category_counts
+
+        dtype = []
+        
+        if self.category_counts:
+            dtype += [(category_name, 'u8') for category_name in self.category_counts]
+
+        self.dtype = np.dtype(dtype)
+
+    def get_at_timestamp(self, timestamp):
+        sample = self.datasources[self.reference_datasource].get_at_timestamp(timestamp)
+        return self[int(np.round(sample.index))]
+
+    def __getitem__(self, key:Any):
+
+        try: #is key iterable?
+            return [self[index] for index in key]
+        except: pass
+
+        raw = np.zeros(1, self.dtype)
+
+        for ds_name in self.dependencies:
+            sample = self.datasources[ds_name][key]
+
+            for category_name in sample.label_names():
+                if category_name in self.category_counts: raw[category_name] += 1
+
+        return Sample(index=key, datasource=self, virtual_raw=raw, virtual_ts=sample.timestamp)
+
